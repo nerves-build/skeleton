b1f76397fa57e35c260165919f995275ad50aaaf
Steven Fuchs <stwf@mac.com>
2017-12-14 19:32:47 -0500

    clone custom system

---
+++ skeleton_system/mix.exs
@@ -0,0 +1,80 @@
+defmodule NervesSystemRpi0.Mixfile do
+  use Mix.Project
+
+  @app :nerves_system_rpi0
+  @version Path.join(__DIR__, "VERSION")
+    |> File.read!
+    |> String.trim
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.4",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  def nerves_package do
+    [
+      type: :system,
+      artifact_url: [
+        "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz"
+      ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      checksum: package_files()
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 0.8", runtime: false },
+      {:nerves_system_br, "0.16.0-2017-11", runtime: false},
+      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 0.12.1", runtime: false},
+      {:nerves_system_linter, "~> 0.2.2", runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves System - Raspberry Pi Zero and Zero W
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Timothy Mecklem", "Frank Hunleth"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "LICENSE",
+      "mix.exs",
+      "nerves_defconfig",
+      "README.md",
+      "VERSION",
+      "rootfs_overlay",
+      "fwup.conf",
+      "cmdline.txt",
+      "linux-4.4.defconfig",
+      "config.txt",
+      "post-createfs.sh"
+    ]
+  end
+end
